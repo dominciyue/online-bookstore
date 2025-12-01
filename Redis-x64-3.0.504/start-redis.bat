b57762aab6b447ad303231cd1bb044fa535c33fd
Dominic <dominicyue@sjtu.edu.cn>
2025-12-01 16:53:37 +0800

    fix redis

---
+++ Redis-x64-3.0.504/start-redis.bat
@@ -0,0 +1,21 @@
+@echo off
+echo ====================================
+echo Starting Redis Server
+echo ====================================
+echo.
+
+redis-server.exe redis.windows.conf
+
+pause
+
+
+
+
+
+
+
+
+
+
+
+
